13c72950264f3a07d068370fe5344ce172bbcb13
khoenv <nguyenkhoe2k4@gmail.com>
2026-05-23 03:23:00 +0700

    nothing

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from matrix_source.trainers.train import Trainer
 from matrix_source.trainers.ppo_stategy import PPOStrategy
 from matrix_source.trainers.d3qn_strategy import D3QNStrategy
 from matrix_source.trainers.rb_sac_cen_stra import RB_SAC_CEN_STRA
+from matrix_source.trainers.d3qn_scaffold_strategy_v2 import D3QNScaffoldStrategy
 
 if __name__ == '__main__':
-    strategy = D3QNStrategy()
+    strategy = D3QNScaffoldStrategy()
     trainer = Trainer(strategy=strategy)
     trainer.train()
